21b8f8944cd8ebc08b75a9830f9fd86465d0d5a7
ihor_hozhenko <lugtrauma@gmail.com>
2025-01-25 17:38:19 +0000

    Added saving contacts

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -5,14 +5,18 @@ import styles from './SearchBox.module.css';
 const SearchBox = ({ value, onChange }) => {
   return (
     <div className={styles.wrapper}>
-      <FaSearch className={styles.icon} />
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        className={styles.input}
-        placeholder="Search contacts by name..."
-      />
+      <label htmlFor="search" className={styles.label}>Find contacts by name</label>
+      <div className={styles.inputWrapper}>
+        <FaSearch className={styles.icon} />
+        <input
+          id="search"
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className={styles.input}
+          placeholder="Search contacts by name..."
+        />
+      </div>
     </div>
   );
 };
